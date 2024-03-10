bbfe639dccabb03b727585be902ec4bbd1d205bc
Onily Valera <70966644+OnilyValera@users.noreply.github.com>
2024-03-09 20:14:28 -0400

    Add files via upload

---
+++ CP2.py
@@ -0,0 +1,31 @@
+import HtmlTestRunner
+import time 
+from selenium import webdriver
+import unittest
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+
+
+class Test(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.driver = webdriver.Chrome()
+        cls.driver.get("https://opensource-demo.orangehrmlive.com/")
+        cls.driver.maximize_window()
+        time.sleep(2)  # Espera de 2 segundos
+
+    def test_login2(self):
+        self.user=self.driver.find_element(By.XPATH,"//input[@name='username']").send_keys("Admin")
+        self.password=self.driver.find_element(By.XPATH,"//input[contains(@type,'password')]").send_keys("admin")
+        self.button=self.driver.find_element(By.XPATH,"//button[@type='submit'][contains(.,'Login')]")
+        self.button.click()
+        time.sleep(7)  # Espera de 5 segundos
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.driver.quit()
+        print("Prueba exitosa. El navegador se ha cerrado correctamente.")
+
+if __name__ == "__main__":
+    unittest.main(testRunner=HtmlTestRunner.HTMLTestRunner(output='reports'))
